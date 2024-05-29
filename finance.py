c141e3440865de63f3cc8430ca45c6fa1102d1fd
Raydog69 <trygve@garatun.no>
2024-05-30 00:47:55 +0200

    p

--- finance.py
+++ finance.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import requests
 import pandas as pd
+import math
+from scipy import stats
 
 def fetch_stock_data(api_key, ticker, start_date, end_date, timeframe):
     url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{timeframe}/{start_date}/{end_date}"
@@ -26,51 +28,127 @@ def plot_graph(data):
 api_key = 'B6aVjCLff5E3KTflUXWVlD7sV3W128hd'
 ticker = 'AAPL'
 start_date = '2023-01-09'
-end_date = '2023-01-12'
-timeframe = '10/minute'
+end_date = '2023-03-09'
+timeframe = '1/hour'
 # Fetch data
 data = fetch_stock_data(api_key, ticker, start_date, end_date, timeframe)
 
-hqm_columns = [
+time_periods = [
+            'Three-Month',
+            'One-Month',
+            '30-Day',
+            '5-Day'
+            ]
+
+ticker_columns = [
                 'Ticker',
-                'Company Name',
                 'Price', 
                 'Number of Shares to Buy', 
-                'Two-Day Price Return', 
-                'Two-Day Return Percentile',
-                'One-Day Price Return',
-                'One-Day Return Percentile',
-                '12-Hour Price Return',
-                '12-Hour Return Percentile',
-                '4-Hour Price Return',
-                '4-Hour Return Percentile',
+                f'{time_periods[0]} Price Return', 
+                f'{time_periods[0]} Return Percentile',
+                f'{time_periods[1]} Price Return',
+                f'{time_periods[1]} Return Percentile',
+                f'{time_periods[2]} Price Return',
+                f'{time_periods[2]} Return Percentile',
+                f'{time_periods[3]} Price Return',
+                f'{time_periods[3]} Return Percentile',
+                'HQM Score'
+                ]
+
+ticker_dataframe = pd.DataFrame(columns = ticker_columns)
+
+hqm_columns = [
+                'Ticker',
+                'Price', 
+                'Number of Shares to Buy', 
+                f'{time_periods[0]} Price Return', 
+                f'{time_periods[0]} Return Percentile',
+                f'{time_periods[1]} Price Return',
+                f'{time_periods[1]} Return Percentile',
+                f'{time_periods[2]} Price Return',
+                f'{time_periods[2]} Return Percentile',
+                f'{time_periods[3]} Price Return',
+                f'{time_periods[3]} Return Percentile',
                 'HQM Score'
                 ]
 
 hqm_dataframe = pd.DataFrame(columns = hqm_columns)
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+IEX_CLOUD_API_TOKEN = 'sk_541d349ea9ee429782bf1e5562bf74fb'
+stocks = pd.read_csv('starter_files/sp_500_stocks.csv')
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+
 # Handle data
 if data:
-    length = len(data['result'])
-    new_series =pd.Series([ticker, 
-                data['companyName'],   
-                float(data['result'][length]['c']),
+    length = len(data['results']) - 1
+    new_series1 =pd.Series([ticker, 
+                float(data['results'][length]['c']),
                 'N/A',
-                float(data['result'][length:]['c']/data['result'][0]['c']),
+                float(data['results'][length]['c']/data['results'][0]['c'] -1),
                 'N/A',
-                float(data['result'][length]['c']/data['result'][length/2]['c']),
+                float(data['results'][length]['c']/data['results'][math.floor(length/3)]['c'] - 1),
                 'N/A',
-                float(data['result'][length]['c']/data['result'][length/4]['c']),
+                float(data['results'][length]['c']/data['results'][math.floor(length/3)]['c'] - 1),
                 'N/A',
-                float(data['result'][length]['c']/data['result'][length/12]['c']),
+                float(data['results'][length]['c']/data['results'][math.floor(length/18)]['c'] - 1),
                 'N/A',
                 'N/A'
                 ], 
                 index = hqm_columns)
-    new_row_df = new_series.to_frame().T
-    hqm_dataframe = pd.concat([hqm_dataframe, new_row_df], ignore_index=True)
+    new_row_df = new_series1.to_frame().T
+    ticker_dataframe = pd.concat([ticker_dataframe, new_row_df], ignore_index=True)
 
-    print(data)
-    plot_graph(data)
+    for symbol_string in symbol_strings:
+    #     print(symbol_strings)
+        batch_api_call_url = f'https://cloud.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+        rel_data = requests.get(batch_api_call_url).json()
+        for symbol in symbol_string.split(','):
+            try:
+                # print(rel_data[symbol]['stats'])
+                new_series2 =pd.Series([symbol, 
+                    float(rel_data[symbol]['quote']['latestPrice']),
+                    'N/A',
+                    float(rel_data[symbol]['stats']['month3ChangePercent']),
+                    'N/A',
+                    float(rel_data[symbol]['stats']['month1ChangePercent']),
+                    'N/A',
+                    float(rel_data[symbol]['stats']['day30ChangePercent']),
+                    'N/A',
+                    float(rel_data[symbol]['stats']['day5ChangePercent']),
+                    'N/A',
+                    'N/A'
+                    ], 
+                    index = hqm_columns)
+                new_row_df = new_series2.to_frame().T
+                hqm_dataframe = pd.concat([hqm_dataframe, new_row_df], ignore_index=True)
+            except:
+                pass
 
-    
+    for row in ticker_dataframe.index:
+        for time_period in time_periods:
+            price_returns = hqm_dataframe[f'{time_period} Price Return']
+            valid_price_returns = [x for x in price_returns if pd.notna(x)]        
+            price_return = ticker_dataframe.loc[row, f'{time_period} Price Return']
+            if pd.notna(price_return):
+                ticker_dataframe.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(valid_price_returns, price_return)/100
+    from statistics import mean
+
+    for row in ticker_dataframe.index:
+        momentum_percentiles = []
+        for time_period in time_periods:
+            momentum_percentiles.append((ticker_dataframe.loc[row, f'{time_period} Return Percentile']))
+        ticker_dataframe.loc[row, 'HQM Score'] = mean(momentum_percentiles)
+
+    position_size = float(100000) / len(ticker_dataframe.index)
+    for i in range(len(ticker_dataframe['Ticker'])):
+        ticker_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / ticker_dataframe['Price'][i])
+
+print(f'HQM for {ticker_dataframe.loc[0, 'Ticker']} is {ticker_dataframe.loc[0, 'HQM Score']}. And the trend for the last 5 days is {ticker_dataframe.loc[0, f'{time_periods[3]} Price Return']}')
---
+++ sk.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import os
+import cv2
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+from sklearn import tree
+
+image_folder = 'archive/Patterns'
+
+# Function to load images and extract features
+def load_images_and_extract_features(folder):
+    data = []
+    labels = []
+    for filename in os.listdir(folder):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            # Load the image
+            img_path = os.path.join(folder, filename)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)  # Load as grayscale
+            if img is not None:
+                # Resize the image to a fixed size (e.g., 64x64)
+                img_resized = cv2.resize(img, (64, 64))
+                # Flatten the image to a 1D array
+                img_flattened = img_resized.flatten()
+                # Append the image data and label
+                data.append(img_flattened)
+                # Extract label from filename (assuming filename format: genre_XXXX.jpg)
+                label = filename.split('_')[0]
+                labels.append(label)
+    return np.array(data), np.array(labels)
+
+x, y = load_images_and_extract_features('archive/Patterns')
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+# model = DecisionTreeClassifier()
+# model.fit(x_train, y_train)
+# joblib.dump(model, 'music-recommender.joblib')
+
+model = joblib.load('music-recommender.joblib')
+
+predictions = model.predict(x_test) 
+score = accuracy_score(y_test, predictions) # [1, 1, 1, 0, 0, 1, 0] = [1, 1, 1, 0, 0, 1, 0]
+print(score)
+
+tree.export_graphviz(model, out_file='music-recommender.dot',
+                     feature_names=[f'pixel{i}' for i in range(x.shape[1])], 
+                     class_names=sorted(set(y)),
+                     label='all',
+                     filled=True)
